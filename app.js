234b14514cfc34e9d5814c5eb548d1ec06858fa3
Vijayasingam Thanasekaran <vijayasingam.thanasekaran@sc.com>
2017-11-23 16:10:02 +0800

    Removed End Point Config

--- app.js
+++ app.js
@@ -29,21 +29,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: APPID,
-    appPassword: APPPWD,
-    endpoint: {
-        refreshEndpoint: 'https://bot-services-app.herokuapp.com/botframework.com/oauth2/v2.0/token',
-        refreshScope: 'https://bot-services-app.herokuapp.com/.default',
-        botConnectorOpenIdMetadata: 'https://bot-services-app.herokuapp.com/v1/.well-known/openidconfiguration',
-        botConnectorIssuer: 'https://api.botframework.com',
-        botConnectorAudience: APPID,
-        emulatorOpenIdMetadata: 'https://login.microsoftonline.com/botframework.com/v2.0/.well-known/openid-configuration',
-        emulatorAudience: APPID,
-        emulatorAuthV31IssuerV1: 'https://sts.windows.net/d6d49420-f39b-4df7-a1dc-d59a935871db/',
-        emulatorAuthV31IssuerV2: 'https://login.microsoftonline.com/d6d49420-f39b-4df7-a1dc-d59a935871db/v2.0',
-        emulatorAuthV32IssuerV1: 'https://sts.windows.net/f8cdef31-a31e-4b4a-93e4-5f571e91255a/',
-        emulatorAuthV32IssuerV2: 'https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a/v2.0',
-        stateEndpoint: 'https://state.botframework.com'
-    }
+    appPassword: APPPWD
 });
 
 // Listen for messages from users 
